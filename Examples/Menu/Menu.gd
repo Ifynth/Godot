4ec765a34d4e67e9f7bbf9343de9745bf3fb2bfe
Ifynth <ifynth@gmail.com>
2018-01-19 17:33:10 +0100

    Adding Controller Support and Menu Controlls

--- Examples/Menu/Menu.gd
+++ Examples/Menu/Menu.gd
@@ -1,15 +1,43 @@
 extends Control
 
 var open
+var cursor
+var cursor_std_pos
+var points
+var points_index = 0
+var label_distance = 50
 
 func _ready():
 	open = false
+	cursor = get_node("Menu Cursor")
+	cursor_std_pos = cursor.get_pos()
+	points = get_node("Labels").get_children()
 	set_process_input(true)
 
 func _input(event):
 	if open and event.is_action_released("ui_accept"):
 		hide()
 		open = false
+		reset()
 	elif !open and event.is_action_released("ui_accept"):
 		show()
 		open = true
+	
+	if open:
+		if event.is_action_pressed("ui_up"):
+			if points_index > 0:
+				points_index -= 1
+			else:
+				points_index = points.size() - 1
+			cursor.set_pos(Vector2(cursor.get_pos().x, cursor_std_pos.y + points_index * label_distance))
+		elif event.is_action_pressed("ui_down"):
+			if points_index < points.size() - 1:
+				points_index += 1
+			else:
+				points_index = 0
+			cursor.set_pos(Vector2(cursor.get_pos().x, cursor_std_pos.y + points_index * label_distance))
+
+func reset():
+	# TODO not neccessary if it is only there if it spawns
+	cursor.set_pos(cursor_std_pos)
+	points_index = 0
--- Examples/Menu/Player.gd
+++ Examples/Menu/Player.gd
@@ -1,20 +1,27 @@
 extends KinematicBody2D
 
 var direction = Vector2(0,0)
+var menu_closed
 
 func _ready():
+	menu_closed = get_node("../CanvasLayer/Menu").is_hidden()
 	set_fixed_process(true)
 
 func _fixed_process(delta):
 	direction = Vector2(0,0)
 	
-	if Input.is_action_pressed("ui_up"):
-		direction = Vector2(0,-1)
-	elif Input.is_action_pressed("ui_down"):
-		direction = Vector2(0,1)
-	elif Input.is_action_pressed("ui_left"):
-		direction = Vector2(-1,0)
-	elif Input.is_action_pressed("ui_right"):
-		direction = Vector2(1,0)
+	if menu_closed:
+		if Input.is_action_pressed("ui_up"):
+			direction = Vector2(0,-1)
+		elif Input.is_action_pressed("ui_down"):
+			direction = Vector2(0,1)
+		elif Input.is_action_pressed("ui_left"):
+			direction = Vector2(-1,0)
+		elif Input.is_action_pressed("ui_right"):
+			direction = Vector2(1,0)
 	
 	move(direction)
+
+func _on_Menu_visibility_changed():
+	# FIX Wird beim ersten Aufruf doppelt ausgeführt ?!?
+	menu_closed = get_node("../CanvasLayer/Menu").is_hidden()
