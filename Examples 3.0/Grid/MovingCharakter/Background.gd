c86882721672e879e8001949b54df3a111ea1955
Ifynth <ifynth@gmail.com>
2018-02-27 18:45:14 +0100

    Set Point, So far so good

--- Examples 3.0/Grid/MovingCharakter/Background.gd
+++ Examples 3.0/Grid/MovingCharakter/Background.gd
@@ -9,3 +9,7 @@ func _draw():
 		draw_line(Vector2(x*size, 0), Vector2(x*size, height*size),"#ffffff",2)
 	for y in range(height + 1):
 		draw_line(Vector2(0, y*size), Vector2(width*size, y*size),"#ffffff",2)
+	
+
+func draw_moving(pos, rang):
+	print(pos)
--- Examples 3.0/Grid/MovingCharakter/Main.gd
+++ Examples 3.0/Grid/MovingCharakter/Main.gd
@@ -1,7 +1,27 @@
 extends TileMap
 
+var FIELD = []
+
+var width = 16
+var height = 9
+var size = 64
+
 func _ready():
 	# unnötig (um perfekte größe zu erschaffen
 	OS.window_size.y = 576
-	pass
+	
+	for x in range(width):
+		FIELD.append([])
+		for y in range(height):
+			FIELD[x].append([])
+			set_Charakter_position_in_field($Good, x, y)
+			set_Charakter_position_in_field($Evil, x, y)
+	print(FIELD)
+	
+	
+
+func set_Charakter_position_in_field(parent, x, y):
+	for child in parent.get_children():
+		if child.position.x == x * size and child.position.y == y * size:
+			FIELD[x][y] = child
 
