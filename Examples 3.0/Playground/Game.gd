6cd2c9f1bf219a9a2fd224b51265076d2f6b8859
Ifynth <ifynth@gmail.com>
2018-03-21 13:41:47 +0100

    Shortened Code @ Game.gd

--- Examples 3.0/Playground/Game.gd
+++ Examples 3.0/Playground/Game.gd
@@ -13,39 +13,28 @@ func createButtonFolder(ignore = [], hidden = false):
 	var button_pos = Vector2(0,0)
 	var path = "res://"
 	var dir = Directory.new()
-	ignore.append(".")
-	ignore.append("..")
 	
 	if dir.open(path) == OK:
-		dir.list_dir_begin()
+		dir.list_dir_begin(true, !hidden)
 		var file_name = dir.get_next()
 		while (file_name != ""):
 			if dir.current_is_dir() and ignore.find(file_name) == -1 and checkMainScene(path + file_name):
-				if hidden:
-					addButton(file_name, button_pos)
-					button_pos.y += 20
-				else:
-					if file_name[0] != ".":
-						addButton(file_name, button_pos)
-						button_pos.y += 20
+				var button = Button.new()
+				button.set_name(file_name)
+				button.set_text(file_name)
+				button.rect_position = button_pos
+				add_child(button)
+				button_pos.y += 20
 			file_name = dir.get_next()
 	else:
 		print("An error occurred when trying to access the path.")
-
-func addButton(name, pos):
-	var button = Button.new()
-	button.set_name(name)
-	button.set_text(name)
-	button.rect_position = pos
-	add_child(button)
-	
 
 func checkMainScene(path):
 	# Check if Folder have a Main Scene
 	var dir = Directory.new()
 	
 	if dir.open(path) == OK:
-		dir.list_dir_begin()
+		dir.list_dir_begin(true, true)
 		var file_name = dir.get_next()
 		while (file_name != ""):
 			if file_name == "Main.tscn":
