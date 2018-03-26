57135b90d9131ba4f2d82375d5d96face5350d55
Ifynth <ifynth@gmail.com>
2018-03-26 11:57:59 +0200

    Added End Fight Button

--- Examples 3.0/Playground/Fight/Main.gd
+++ Examples 3.0/Playground/Fight/Main.gd
@@ -1,27 +1,39 @@
 extends Node
 
-#TODO start Animation, left hit right
-# left -> attacker (Charakter.attacker)
-# right -> defender (Charakter.defender)
+"""
+enum {LEFT, RIGHT}
+var start = LEFT
+"""
 
 func _ready():
 	$"UI/Test Buttons/Test".connect("button_down", self, "onButtonTestPressed")
 	$"UI/Test Buttons/Left Attack".connect("button_down", self, "simulateAttack", [Charakter.attacker, Charakter.defender, $UI/Defender])
 	$"UI/Test Buttons/Right Attack".connect("button_down", self, "simulateAttack", [Charakter.defender, Charakter.attacker, $UI/Attacker])
+	$"UI/Test Buttons/End Fight".connect("button_down", self, "endFight")
 	
-
-func simulateAttack(attacker, defender, ui):
-	# TODO (signal?)
-	# HACK how to know which UI to use
-	ui.takeDamage(defender.takeDamage(attacker.ATK))
-
-func onButtonTestPressed():
-	#print("Test")
 	print("Fighting Chars: ")
 	print("  Attacker: ", Charakter.attacker.get_name())
 	setUI(Charakter.attacker, $UI/Attacker)
 	print("  Defender: ", Charakter.defender.get_name())
 	setUI(Charakter.defender, $UI/Defender)
+	
+
+func simulateAttack(attacker, defender, ui):
+	ui.takeDamage(defender.takeDamage(attacker.ATK))
+
+func onButtonTestPressed():
+	print("Test")
+	"""
+	# OS.delay_msec(4000)
+	# start Hitting
+	if start == LEFT:
+		simulateAttack(Charakter.attacker, Charakter.defender, $UI/Defender)
+	else:
+		simulateAttack(Charakter.defender, Charakter.attacker, $UI/Attacker)
+	"""
+
+func endFight():
+	get_tree().change_scene("res://Game.tscn")
 
 func setUI(charakter, ui):
 	print("Setting up UI for: ", charakter.get_name())
