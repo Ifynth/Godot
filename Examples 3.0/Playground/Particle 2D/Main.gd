80062d2dc9b66f1f6d97cffc3cd63cff195d6f57
Ifynth <ifynth@gmail.com>
2018-05-14 15:18:41 +0200

    Add a Particle 2D Test Scene

---
+++ Examples 3.0/Playground/Particle 2D/Main.gd
@@ -0,0 +1,18 @@
+extends Node
+
+export var SPEED = 200
+
+var velocity = Vector2()
+
+func _process(delta):
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	
+	$Char.position += velocity.normalized() * delta * SPEED
